709d7a501a2292d000dd8181ae156987708b1e11
Zachary Rizzuto <zachrizzuto@gmail.com>
2023-07-16 01:52:28 -0500

    Add page two
    created page two for artworks

---
+++ js/pageTwo.js
@@ -0,0 +1,77 @@
+const container = document.getElementById('content');
+const open = '[data-open]';
+
+const artData = fetch('https://api.artic.edu/api/v1/artworks?page=2&limit=12')
+	.then(res => res.json())
+	.then(data => data)
+	.catch((err) => console.log(err));
+
+const createCard = (imgId, title, artist, url) => {
+	const urlTail = '/full/843,/0/default.jpg';
+
+	const card = document.createElement('div');
+	card.classList.add('art-card');
+
+	const imgContainer = document.createElement('div');
+	imgContainer.classList.add('img-container');
+
+	const img = document.createElement('img');
+	img.src = url + imgId + urlTail;
+
+	const infoOne = document.createElement('p');
+	infoOne.innerHTML = `Title: ${title}`;
+
+	const infoTwo = document.createElement('p');
+	infoTwo.innerHTML = `Artist: ${artist}`;
+
+	container.appendChild(card);
+	card.appendChild(imgContainer);
+	imgContainer.appendChild(img);
+	card.appendChild(infoOne);
+	card.appendChild(infoTwo);
+}
+
+const createModal = (imgUrl) => {
+	const modalWrap = document.createElement('div');
+	modalWrap.classList.add('modal-wrap');
+	modalWrap.dataset.toggle = 'open';
+	modalWrap.addEventListener('click', (e) => {
+		closeModal(e.target);
+	});
+
+	const modalImgWrap = document.createElement('div');
+	modalImgWrap.classList.add('modal-img-wrap');
+
+	const modalImg = document.createElement('img');
+	modalImg.classList.add('modal-img');
+	modalImg.src = imgUrl;
+
+	container.appendChild(modalWrap);
+	modalWrap.appendChild(modalImgWrap);
+	modalImgWrap.appendChild(modalImg);
+}
+
+const closeModal = (modal) => {
+	modal.remove();
+}
+
+const fromDataToCard = async () => {
+	let loadedArt = await artData;
+
+	await loadedArt.data.forEach((piece) => {
+		createCard(piece.image_id, piece.title, piece.artist_title, 'https://www.artic.edu/iiif/2/');
+	})
+	console.log('Cards done.')
+
+	container.addEventListener('click', function(e) {
+		let elm = e.target;
+		elm.tagName === 'IMG' ? createModal(e.target.src) : null;
+	})
+}
+
+
+
+fromDataToCard();
+
+
+
