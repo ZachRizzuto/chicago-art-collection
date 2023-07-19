cc10241da8fe5521eb89a0d88303899fb3dbeb76
Zachary Rizzuto <zachrizzuto@gmail.com>
2023-07-18 19:30:14 -0500

    Add fav buttons and starting functionality
    Added buttons so they can add the data to an array and removed page one and two for simplicity and redundancy issues

--- js/index.js
+++ js/index.js
@@ -1,13 +1,15 @@
+const favs = [];
+
 const container = document.getElementById('content');
 const open = '[data-open]';
 const loader = document.getElementById('loading');
 
-const artData = fetch('https://api.artic.edu/api/v1/artworks?fields=title,artist_title,image_id,date_start,date_end')
+const artData = fetch('https://api.artic.edu/api/v1/artworks?fields=title,artist_title,image_id,date_start,date_end,id')
 	.then(res => res.json())
 	.then(data => data)
 	.catch((err) => console.log(err));
 
-	const createCard = (imgId, title, artist, url, dateStart, dateEnd) => {
+const createCard = (imgId, title, artist, url, dateStart, dateEnd, artId) => {
 		const urlTail = '/full/843,/0/default.jpg';
 	
 		const card = document.createElement('div');
@@ -16,6 +18,7 @@ const artData = fetch('https://api.artic.edu/api/v1/artworks?fields=title,artist
 	
 		const favBtn = document.createElement('button');
 		favBtn.classList.add('favBtn');
+		favBtn.dataset.addfav = 'true';
 	
 		const btnIcon = document.createElement('i');
 		btnIcon.classList.add('fa-regular');
@@ -42,6 +45,10 @@ const artData = fetch('https://api.artic.edu/api/v1/artworks?fields=title,artist
 		artist === null 
 		? infoTwo.innerHTML = `Creator: Unknown`
 		: infoTwo.innerHTML = `Creator: ${artist}`;
+
+		const infoThree = document.createElement('p');
+		infoThree.innerHTML = `Artwork Id: ${artId}`;
+		infoThree.classList.add('idInfo');
 	
 		container.appendChild(card);
 		card.appendChild(favBtn);
@@ -51,7 +58,8 @@ const artData = fetch('https://api.artic.edu/api/v1/artworks?fields=title,artist
 		card.appendChild(infoOne);
 		card.appendChild(yearsWorked);
 		card.appendChild(infoTwo);
-	}
+		card.appendChild(infoThree);
+}
 
 const createModal = (imgUrl) => {
 	const modalWrap = document.createElement('div');
@@ -107,7 +115,7 @@ const fromDataToHTML = async () => {
 	displayLoading();
 
 	await loadedArt.data.forEach((piece) => {
-		createCard(piece.image_id, piece.title, piece.artist_title, 'https://www.artic.edu/iiif/2/', piece.date_start, piece.date_end);
+		createCard(piece.image_id, piece.title, piece.artist_title, 'https://www.artic.edu/iiif/2/', piece.date_start, piece.date_end, piece.id);
 	})
 
 	stopLoading();
@@ -115,12 +123,32 @@ const fromDataToHTML = async () => {
 
 	showYearsWorked();
 	showFavs();
+	addFav();
 }
 
 container.addEventListener('click', (e) => {
 	let elm = e.target;
 	elm.tagName === 'IMG' ? createModal(e.target.src) : null;
 })
+
+const addFav = async () => {
+	let loadedArt = await artData;
+	container.addEventListener('click', function (e) {
+		let elm = e.target;
+		if (elm.dataset.addfav === 'true') {
+			let elmparent = e.target.parentElement;
+			let id = elmparent.querySelector('.idInfo').textContent.match(/\d/g).join("");
+			let piece = loadedArt.data;
+			for (i of piece) {
+				if (i.id == id && !favs.includes(i)) {
+					favs.push(i);
+					console.log(favs);
+				}
+			}
+		}
+	})
+}
+
 
 const showFavs = () => {
 	let btn = document.getElementById('addFav')
